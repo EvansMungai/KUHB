4b49e2ad8499cb86d6e020ffde056f909a6cc51e
Evans Mungai <thetracker254@gmail.com>
2024-02-02 17:57:40 +0300

    Set up login and signin page

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/src/index.html');
+    res.sendFile(__dirname + '/src/booking.html');
+})
+app.get('/signin', (req, res)=> {
+    res.sendFile(__dirname + '/src/signin.html');
+})
+app.get('/login', (req, res)=> {
+    res.sendFile(__dirname + '/src/login.html')
 })
 app.listen(port, (err) => {
     if (err) {
