a45e8d76fea2336ed140403771d139bfe4db70fe
Evans Mungai <thetracker254@gmail.com>
2024-02-02 16:54:02 +0300

    Set up Booking form

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname + '/public/stylesheets'));
+app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/src/index.html');
 })
