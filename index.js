7f174cd54ac1a5fd2ca5ad7d03adc238aaad357a
Evans Mungai <thetracker254@gmail.com>
2024-02-02 15:36:12 +0300

    Set up Booking Form

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res)=>{
-    res.send("Welcome to Kimathi University Booking system")
+app.use(express.static(__dirname + '/public/stylesheets'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/src/index.html');
 })
-app.listen(port, ()=>{
-    console.log("Server is running on port "+port);
+app.listen(port, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Server is running on port " + port);
+    }
 })
