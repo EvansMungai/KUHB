eaa93fcf2e9b46025ba64afecc61180dc46b3329
Evans Mungai <thetracker254@gmail.com>
2024-02-14 19:27:56 +0300

    Update housekeeper and matron navbar

--- server/controllers/matronController.js
+++ server/controllers/matronController.js
@@ -14,3 +14,18 @@ exports.showSuccessfulApplications = async (req, res) => {
         }
     })
 }
+exports.allocateRooms = async (req, res) => {
+    db.query('select * from applications where status = "Successful"', (err, result) => {
+        if (err) {
+            return err;
+        } else {
+            res.render('./layouts/matronAllocateRooms', {
+                link1: "Successful Applications",
+                link1Href: "/matron",
+                link2: "Allocate Rooms",
+                link2Href: "/matron/allocaterooms",
+                sampleData: result,
+            })
+        }
+    })
+}
