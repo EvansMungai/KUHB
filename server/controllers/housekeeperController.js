8f9bde7747131f5e01c58ee1754279ff8e6e126d
Evans Mungai <thetracker254@gmail.com>
2024-02-14 15:22:07 +0300

    Set up housekeeper dashboard and controller

--- server/controllers/housekeeperController.js
+++ server/controllers/housekeeperController.js
@@ -6,6 +6,25 @@ exports.showApplications = async (req, res) => {
             return err;
         } else {
             res.render('./layouts/housekeeperDashboard', {
+                link1: "Review applications",
+                link1Href: "/housekeeper",
+                link2: "Successful Applications",
+                link2Href: "/housekeeper/successfulapplications",
+                sampleData: result,
+            })
+        }
+    })
+}
+exports.showSuccessfulApplications = async (req, res) => {
+    db.query('select * from applications where Status = "Accepted"', (err, result) => {
+        if (err) {
+            return err;
+        } else {
+            res.render('./layouts/housekeeperDashboard', {
+                link1: "Review applications",
+                link1Href: "/housekeeper",
+                link2: "Successful Applications",
+                link2Href: "/housekeeper/successfulapplications",
                 sampleData: result,
             })
         }
--- server/routes/housekeeperDashboard.js
+++ server/routes/housekeeperDashboard.js
@@ -2,4 +2,5 @@ const housekeeperController = require('../controllers/housekeeperController');
 
 module.exports = (app)=>{
     app.get('/housekeeper', housekeeperController.showApplications);
+    app.get('/housekeeper/successfulapplications', housekeeperController.showSuccessfulApplications);
 }
