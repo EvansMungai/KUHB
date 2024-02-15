0f288334cacea6944a96f3412d9302451b3d4a90
Evans Mungai <thetracker254@gmail.com>
2024-02-15 18:08:27 +0300

    Set up allocation procedure in matron controller

--- server/controllers/matronController.js
+++ server/controllers/matronController.js
@@ -5,25 +5,70 @@ exports.showSuccessfulApplications = async (req, res) => {
             return err;
         } else {
             res.render('./layouts/matronDashboard', {
-                link1: "Successful Applications",
+                link1: "Review Allocations",
                 link1Href: "/matron",
-                link2: "Allocate Rooms",
-                link2Href: "/matron/allocaterooms",
+                link2: "Allocated Rooms",
+                link2Href: "/matron/occupiedrooms",
                 sampleData: result,
             })
         }
     })
 }
-exports.allocateRooms = async (req, res) => {
-    db.query('select * from applications where status = "Accepted"', (err, result) => {
+exports.viewAllocations = async (req, res) => {
+    var id = req.params.id;
+    db.query('select * from applications where ApplicationNo=?;',id, (err, result)=>{
         if (err) {
-            return err;
+            throw err
         } else {
-            res.render('./layouts/matronAllocateRooms', {
-                link1: "Successful Applications",
+            db.query('select * from rooms', (err, result2)=>{
+                res.render('./layouts/matronReviewAllocations', {
+                    link1: "Review allocations",
+                    link1Href: "/matron",
+                    link2: "Allocated Rooms",
+                    link2Href: "/matron/occupiedrooms",
+                    sampleData: result,
+                    roomsData: result2
+                });
+            })
+        }
+    })
+}
+exports.allocateRoom = async (req, res)=>{
+    var id = req.params.id;
+    var roomNo = req.body.RoomNo;
+    let params = {
+        RoomNo: roomNo
+    }
+    db.query('Update applications set? where ApplicationNo=?',[params, id], (err, result)=>{
+        if (err) {
+            throw err
+        } else {
+            db.query('select * from applications where Status = "Accepted"', (err, result)=>{
+                if (err) {
+                    throw err
+                } else {
+                    res.render('./layouts/matronDashboard', {
+                        link1: "Review Allocations",
+                        link1Href: "/matron",
+                        link2: "Allocated Rooms",
+                        link2Href: "/matron/occupiedrooms",
+                        sampleData: result,
+                    })
+                }
+            })
+        }
+    })
+}
+exports.viewAllocatedRooms = async (req, res)=>{
+    db.query('select RegistrationNo, RoomNo from applications', (err, result)=>{
+        if (err) {
+            throw err
+        } else {
+            res.render('./layouts/allocatedRooms', {
+                link1: "Review Allocations",
                 link1Href: "/matron",
-                link2: "Allocate Rooms",
-                link2Href: "/matron/allocaterooms",
+                link2: "Allocated Rooms",
+                link2Href: "/matron/occupiedrooms",
                 sampleData: result,
             })
         }
--- server/routes/matronDashboard.js
+++ server/routes/matronDashboard.js
@@ -2,5 +2,7 @@ const matronController = require('../controllers/matronController');
 
 module.exports = (app)=>{
     app.get('/matron', matronController.showSuccessfulApplications);
-    app.get('/matron/allocaterooms', matronController.showSuccessfulApplications);
+    app.get('/matron/viewallocations/:id', matronController.viewAllocations);
+    app.post('/matron/reviewallocations/:id', matronController.allocateRoom);
+    app.get('/matron/occupiedrooms', matronController.viewAllocatedRooms);
 }
