e843a54b63ed3122b8773eb10036bf253b7de3d4
Evans Mungai <thetracker254@gmail.com>
2024-02-15 10:04:39 +0300

    Set register rooms and register hostels feature

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,5 +1,45 @@
 const db = require('../config/database');
 
+exports.hostelsRegistrationForm = async (req,res)=>{
+    res.render('./layouts/adminRegisterHostels');
+}
+exports.registerHostels = async (req, res)=>{
+    var hostelNo = req.body.HostelNo;
+    var hostelName = req.body.HostelName;
+    var capacity = req.body.Capacity;
+    var type = req.body.Type;
+    let params = {
+        HostelNo: hostelNo,
+        HostelName: hostelName,
+        Capacity: capacity,
+        Type: type
+    }
+    db.query('insert into hostels set?', params, (err, result)=>{
+        if (err) {
+            throw err
+        } else {
+            res.redirect('/admin')
+        }
+    })
+}
+exports.roomsRegistrationForm = async (req, res)=>{
+    res.render('./layouts/adminRegisterRooms');
+}
+exports.registerRooms = async (req, res)=>{
+    var roomNo = req.body.RoomNo;
+    var hostelNo = req.body.HostelNo;
+    let params = {
+        RoomNo: roomNo,
+        HostelNo: hostelNo
+    }
+    db.query('insert into rooms set?', params, (err, result)=>{
+        if (err) {
+            throw err
+        } else {
+            res.redirect('/admin/rooms')
+        }
+    })
+}
 exports.showHostels = async (req, res) => {
     db.query('select * from hostels', (err, result) => {
         if (err) {
--- server/routes/adminDashboard.js
+++ server/routes/adminDashboard.js
@@ -1,7 +1,11 @@
 const adminController = require('../controllers/adminController');
 
 module.exports = (app)=>{
-    app.get('/admin', adminController.showHostels)
+    app.get('/admin', adminController.showHostels);
+    app.get('/admin/registerhostels', adminController.hostelsRegistrationForm);
+    app.post('/admin/registerhostels', adminController.registerHostels);
+    app.get('/admin/registerrooms', adminController.roomsRegistrationForm);
+    app.post('/admin/registerrooms', adminController.registerRooms);
     app.get('/admin/rooms', adminController.showRooms);
     app.get('/admin/students', adminController.showStudents);
     app.get('/admin/users', adminController.showUsers);
