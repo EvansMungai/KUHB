a17ab6e421a75127eb2b741559f71b4608258f15
Evans Mungai <thetracker254@gmail.com>
2024-02-27 15:47:27 +0300

    Hide login information

--- server/config/database.js
+++ server/config/database.js
@@ -1,9 +1,12 @@
 const { createPool } = require('mysql2');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env' })
 const pool = createPool({
-    host: "localhost",
-    user: "uhb",
-    password: "uhbadmin",
-    database: "uhb",
+    host: process.env.Host,
+    user: process.env.User,
+    password: process.env.Password,
+    database: process.env.Database,
     connectionLimit: 10
 })
 
