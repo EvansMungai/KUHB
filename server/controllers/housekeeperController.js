bd1c3fdaf671019fafeef2af1378ce423f569adc
Evans Mungai <thetracker254@gmail.com>
2024-02-28 14:20:49 +0300

    Limit housekeeper to access system only when logged in

--- server/controllers/housekeeperController.js
+++ server/controllers/housekeeperController.js
@@ -1,43 +1,51 @@
 const db = require('../config/database');
 
 exports.showApplications = async (req, res) => {
-    db.query('select * from applications', (err, result) => {
-        if (err) {
-            return err;
-        } else {
-            res.render('./layouts/housekeeperDashboard', {
-                link1: "Review applications",
-                link1Href: "/housekeeper",
-                link2: "Successful Applications",
-                link2Href: "/housekeeper/successfulapplications",
-                sampleData: result,
-            })
-        }
-    })
+    if (req.session.user) {
+        db.query('select * from applications', (err, result) => {
+            if (err) {
+                return err;
+            } else {
+                res.render('./layouts/housekeeperDashboard', {
+                    link1: "Review applications",
+                    link1Href: "/housekeeper",
+                    link2: "Successful Applications",
+                    link2Href: "/housekeeper/successfulapplications",
+                    sampleData: result,
+                })
+            }
+        })
+    } else {
+        res.redirect('/login');
+    }
 }
-exports.viewApplications = async (req, res)=>{
-    var id = req.params.id;
-    db.query('select * from applications where ApplicationNo=?;',id, (err, result)=>{
-        if (err) {
-            throw err
-        } else {
-            res.render('./layouts/housekeeperReviewApplications.ejs', {
-                link1: "Review applications",
-                link1Href: "/housekeeper",
-                link2: "Successful Applications",
-                link2Href: "/housekeeper/successfulapplications",
-                sampleData: result,
-            });
-        }
-    })
+exports.viewApplications = async (req, res) => {
+    if (req.session.user) {
+        var id = req.params.id;
+        db.query('select * from applications where ApplicationNo=?;', id, (err, result) => {
+            if (err) {
+                throw err
+            } else {
+                res.render('./layouts/housekeeperReviewApplications.ejs', {
+                    link1: "Review applications",
+                    link1Href: "/housekeeper",
+                    link2: "Successful Applications",
+                    link2Href: "/housekeeper/successfulapplications",
+                    sampleData: result,
+                });
+            }
+        })
+    } else {
+        res.redirect('/login')
+    }
 }
-exports.reviewApplictions = async (req, res)=>{
+exports.reviewApplictions = async (req, res) => {
     var status = req.body.Status;
     var id = req.params.appNo;
     let params = {
         Status: status
     }
-    db.query('Update applications set? where ApplicationNo=?',[params, id], (err, result)=>{
+    db.query('Update applications set? where ApplicationNo=?', [params, id], (err, result) => {
         if (err) {
             throw err
         } else {
@@ -58,17 +66,21 @@ exports.reviewApplictions = async (req, res)=>{
     })
 }
 exports.showSuccessfulApplications = async (req, res) => {
-    db.query('select * from applications where Status = "Accepted"', (err, result) => {
-        if (err) {
-            return err;
-        } else {
-            res.render('./layouts/housekeeperSuccessfulApplications', {
-                link1: "Review applications",
-                link1Href: "/housekeeper",
-                link2: "Successful Applications",
-                link2Href: "/housekeeper/successfulapplications",
-                sampleData: result,
-            })
-        }
-    })
+    if (req.session.user) {
+        db.query('select * from applications where Status = "Accepted"', (err, result) => {
+            if (err) {
+                return err;
+            } else {
+                res.render('./layouts/housekeeperSuccessfulApplications', {
+                    link1: "Review applications",
+                    link1Href: "/housekeeper",
+                    link2: "Successful Applications",
+                    link2Href: "/housekeeper/successfulapplications",
+                    sampleData: result,
+                })
+            }
+        })
+    } else {
+        res.redirect('/login');
+    }
 }
