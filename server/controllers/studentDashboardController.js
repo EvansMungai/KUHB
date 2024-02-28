c794fbfe701b05ed75d969e9260a632ddb8dd554
Evans Mungai <thetracker254@gmail.com>
2024-02-28 14:26:20 +0300

    Limit student to access system only when logged in

--- server/controllers/studentDashboardController.js
+++ server/controllers/studentDashboardController.js
@@ -1,14 +1,18 @@
 const db = require('../config/database');
 exports.homepage = async (req, res) => {
-    db.query('select * from students', (err, result) => {
-        if (err) {
-            return err;
-        } else {
-            res.render('./layouts/studentdashboard', {
-                sampleData: result,
-            })
-        }
-    })
+    if (req.session.user) {
+        db.query('select * from students', (err, result) => {
+            if (err) {
+                return err;
+            } else {
+                res.render('./layouts/studentdashboard', {
+                    sampleData: result,
+                })
+            }
+        })
+    } else {
+        res.redirect('/login');
+    }
 }
 exports.postBookingData = async (req, res) => {
     var hostelName = req.body.hostelName;
@@ -18,15 +22,19 @@ exports.postBookingData = async (req, res) => {
     console.log(room);
 }
 exports.application = async (req, res) => {
-    db.query('select hostels.HostelNo, hostels.HostelName, hostels.Capacity, hostels.Type, rooms.RoomNo from hostels left join rooms on hostels.HostelNo = rooms.HostelNo', (err, result) => {
-        if (err) {
-            return err;
-        } else {
-            res.render('./layouts/application', {
-                sampleData: result,
-            })
-        }
-    })
+    if (req.session.user) {
+        db.query('select hostels.HostelNo, hostels.HostelName, hostels.Capacity, hostels.Type, rooms.RoomNo from hostels left join rooms on hostels.HostelNo = rooms.HostelNo', (err, result) => {
+            if (err) {
+                return err;
+            } else {
+                res.render('./layouts/application', {
+                    sampleData: result,
+                })
+            }
+        })
+    } else {
+        res.redirect('/login');
+    }
 }
 exports.sendApplication = async (req, res) => {
     var registrationNo = req.body.RegNo;
@@ -76,26 +84,34 @@ exports.sendApplication = async (req, res) => {
     })
 }
 exports.applicationDetails = async (req, res) => {
-    db.query('select * from applications', (err, result) => {
-        if (err) {
-            return err;
-        } else {
-            res.render('./layouts/studentApplicationDetails', {
-                sampleData: result
-            })
-        }
-    })
+    if (req.sesssion.user) {
+        db.query('select * from applications', (err, result) => {
+            if (err) {
+                return err;
+            } else {
+                res.render('./layouts/studentApplicationDetails', {
+                    sampleData: result
+                })
+            }
+        })
+    } else {
+        res.redirect('/login');
+    }
 }
 exports.accommodationDetails = async (req, res) => {
-    db.query('select * from applications where status = "Accepted"', (err, result) => {
-        if (err) {
-            return err;
-        } else {
-            res.render('./layouts/studentAccommodationDetails', {
-                sampleData: result
-            })
-        }
-    })
+    if (req.session.user) {
+        db.query('select * from applications where status = "Accepted"', (err, result) => {
+            if (err) {
+                return err;
+            } else {
+                res.render('./layouts/studentAccommodationDetails', {
+                    sampleData: result
+                })
+            }
+        })
+    } else {
+        res.redirect('/login')
+    }
 }
 // exports.userDetails = async (req, res)=>{
 //     db.query('select * from users', (err, result)=>{
