508afbb3844bc9d093238b1194272bf3a88acf8c
Evans Mungai <thetracker254@gmail.com>
2024-02-28 14:58:33 +0300

    Set admin pages to be accessed by admins only

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -2,167 +2,225 @@ const db = require('../config/database');
 
 exports.hostelsRegistrationForm = async (req, res) => {
     if (req.session.user) {
-        res.render('./layouts/adminRegisterHostels');
+        if (req.session.role === "Admin") {
+            res.render('./layouts/adminRegisterHostels');
+        } else {
+            res.send("Unauthorized access")
+        }
     } else {
         res.redirect('/login')
     }
 }
 exports.registerHostels = async (req, res) => {
-    var hostelNo = req.body.HostelNo;
-    var hostelName = req.body.HostelName;
-    var capacity = req.body.Capacity;
-    var type = req.body.Type;
-    let params = {
-        HostelNo: hostelNo,
-        HostelName: hostelName,
-        Capacity: capacity,
-        Type: type
-    }
-    db.query('insert into hostels set?', params, (err, result) => {
-        if (err) {
-            throw err
+    if (req.session.user) {
+        if (req.session.role === "Admin") {
+            var hostelNo = req.body.HostelNo;
+            var hostelName = req.body.HostelName;
+            var capacity = req.body.Capacity;
+            var type = req.body.Type;
+            let params = {
+                HostelNo: hostelNo,
+                HostelName: hostelName,
+                Capacity: capacity,
+                Type: type
+            }
+            db.query('insert into hostels set?', params, (err, result) => {
+                if (err) {
+                    throw err
+                } else {
+                    res.redirect('/admin')
+                }
+            })
         } else {
-            res.redirect('/admin')
+            res.send("Unauthorized access")
         }
-    })
+    }
 }
 exports.showHostels = async (req, res) => {
     if (req.session.user) {
-        db.query('select * from hostels', (err, result) => {
-            if (err) {
-                return err;
-            } else {
-                res.render('./layouts/adminDashboard', {
-                    sampleData: result,
-                })
-            }
-        })
+        if (req.session.role === "Admin") {
+            db.query('select * from hostels', (err, result) => {
+                if (err) {
+                    return err;
+                } else {
+                    res.render('./layouts/adminDashboard', {
+                        sampleData: result,
+                    })
+                }
+            })
+        } else {
+            res.send("Unauthorized access")
+        }
     } else {
         res.redirect('/login')
     }
 }
 exports.roomsRegistrationForm = async (req, res) => {
     if (req.session.user) {
-        res.render('./layouts/adminRegisterRooms');
+        if (req.session.role === "Admin") {
+            res.render('./layouts/adminRegisterRooms');
+        } else {
+            res.send("Unauthorized access")
+        }
     } else {
         res.redirect('/login')
     }
 }
 exports.registerRooms = async (req, res) => {
-    var roomNo = req.body.RoomNo;
-    var hostelNo = req.body.HostelNo;
-    let params = {
-        RoomNo: roomNo,
-        HostelNo: hostelNo
-    }
-    db.query('insert into rooms set?', params, (err, result) => {
-        if (err) {
-            throw err
+    if (req.session.user) {
+        if (req.session.role === "Admin") {
+            var roomNo = req.body.RoomNo;
+            var hostelNo = req.body.HostelNo;
+            let params = {
+                RoomNo: roomNo,
+                HostelNo: hostelNo
+            }
+            db.query('insert into rooms set?', params, (err, result) => {
+                if (err) {
+                    throw err
+                } else {
+                    res.redirect('/admin/rooms')
+                }
+            })
         } else {
-            res.redirect('/admin/rooms')
+            res.send("Unauthorized access")
         }
-    })
+    }
 }
 exports.showRooms = async (req, res) => {
     if (req.session.user) {
-        db.query('select * from rooms', (err, result) => {
-            if (err) {
-                throw err;
-            } else {
-                res.render('./layouts/adminDashboardRooms', {
-                    sampleData: result,
-                })
-            }
-        })
+        if (req.session.role === "Admin") {
+            db.query('select * from rooms', (err, result) => {
+                if (err) {
+                    throw err;
+                } else {
+                    res.render('./layouts/adminDashboardRooms', {
+                        sampleData: result,
+                    })
+                }
+            })
+        } else {
+            res.send("Unauthorized access")
+        }
     } else {
         res.redirect('/login')
     }
 }
 exports.studentsRegistrationForm = async (req, res) => {
     if (req.session.user) {
-        res.render('./layouts/adminRegisterStudents');
+        if (req.session.role === "Admin") {
+            res.render('./layouts/adminRegisterStudents');
+        } else {
+            res.send("Unauthorized access")
+        }
     } else {
         res.redirect('/login')
     }
 }
 exports.registerStudents = async (req, res) => {
-    var registrationNo = req.body.RegistrationNo;
-    var surname = req.body.Surname;
-    var firstName = req.body.FirstName;
-    var secondName = req.body.SecondName;
-    var gender = req.body.gender;
-    let params = {
-        RegNO: registrationNo,
-        Surname: surname,
-        FirstName: firstName,
-        SecondName: secondName,
-        Gender: gender
-    }
-    db.query('insert into students set?', params, (err, result) => {
-        if (err) {
-            throw err
+    if (req.session.user) {
+        if (req.session.role === "Admin") {
+            var registrationNo = req.body.RegistrationNo;
+            var surname = req.body.Surname;
+            var firstName = req.body.FirstName;
+            var secondName = req.body.SecondName;
+            var gender = req.body.gender;
+            let params = {
+                RegNO: registrationNo,
+                Surname: surname,
+                FirstName: firstName,
+                SecondName: secondName,
+                Gender: gender
+            }
+            db.query('insert into students set?', params, (err, result) => {
+                if (err) {
+                    throw err
+                } else {
+                    res.redirect('/admin/students')
+                }
+            })
         } else {
-            res.redirect('/admin/students')
+            res.send("Unauthorized access")
         }
-    })
+    } else {
+        res.redirect('/login')
+    }
 }
 exports.showStudents = async (req, res) => {
     if (req.session.user) {
-        db.query('select * from students', (err, result) => {
-            if (err) {
-                throw err;
-            } else {
-                res.render('./layouts/adminDashboardStudents', {
-                    sampleData: result,
-                })
-            }
-        })
+        if (req.session.role === "Admin") {
+            db.query('select * from students', (err, result) => {
+                if (err) {
+                    throw err;
+                } else {
+                    res.render('./layouts/adminDashboardStudents', {
+                        sampleData: result,
+                    })
+                }
+            })
+        } else {
+            res.send("Unauthorized access")
+        }
     } else {
         res.redirect('/login');
     }
 }
 exports.showUsers = async (req, res) => {
     if (req.session.user) {
-        db.query('select * from users', (err, result) => {
-            if (err) {
-                throw err;
-            } else {
-                res.render('./layouts/adminDashboardUsers', {
-                    sampleData: result,
-                })
-            }
-        })
+        if (req.session.role === "Admin") {
+            db.query('select * from users', (err, result) => {
+                if (err) {
+                    throw err;
+                } else {
+                    res.render('./layouts/adminDashboardUsers', {
+                        sampleData: result,
+                    })
+                }
+            })
+        } else {
+            res.send("Unauthorized access")
+        }
     } else {
         res.redirect('/login')
     }
 }
 exports.showUserDetails = async (req, res) => {
     if (req.session.user) {
-        var id = req.params.id;
-        db.query('select * from users where Username=?', id, (err, result) => {
-            if (err) {
-                throw err
-            } else {
-                res.render('./layouts/adminUpdateUserRole', {
-                    sampleData: result
-                });
-            }
-        })
-    } else{
+        if (req.session.role === "Admin") {
+            var id = req.params.id;
+            db.query('select * from users where Username=?', id, (err, result) => {
+                if (err) {
+                    throw err
+                } else {
+                    res.render('./layouts/adminUpdateUserRole', {
+                        sampleData: result
+                    });
+                }
+            })
+        } else {
+            res.send("Unauthorized access")
+        }
+    } else {
         res.redirect('/login')
     }
 }
 exports.changeRoles = async (req, res) => {
-    var id = req.params.username;
-    var role = req.body.role;
-    let params = {
-        Role: role
-    }
-    db.query('update users set? where Username = ?;', [params, id], (err, result) => {
-        if (err) {
-            throw err
+    if (req.session.user) {
+        if (req.session.role === "Admin") {
+            var id = req.params.username;
+            var role = req.body.role;
+            let params = {
+                Role: role
+            }
+            db.query('update users set? where Username = ?;', [params, id], (err, result) => {
+                if (err) {
+                    throw err
+                } else {
+                    res.redirect('/admin/users')
+                }
+            })
         } else {
-            res.redirect('/admin/users')
+            res.send("Unauthorized access")
         }
-    })
+    }
 }
