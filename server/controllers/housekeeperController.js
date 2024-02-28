fe796d621854df878a3980c15333999bee714cd6
Evans Mungai <thetracker254@gmail.com>
2024-02-28 14:51:56 +0300

    Set housekeeper pages to be accessed only by the housekeeper

--- server/controllers/housekeeperController.js
+++ server/controllers/housekeeperController.js
@@ -2,53 +2,7 @@ const db = require('../config/database');
 
 exports.showApplications = async (req, res) => {
     if (req.session.user) {
-        db.query('select * from applications', (err, result) => {
-            if (err) {
-                return err;
-            } else {
-                res.render('./layouts/housekeeperDashboard', {
-                    link1: "Review applications",
-                    link1Href: "/housekeeper",
-                    link2: "Successful Applications",
-                    link2Href: "/housekeeper/successfulapplications",
-                    sampleData: result,
-                })
-            }
-        })
-    } else {
-        res.redirect('/login');
-    }
-}
-exports.viewApplications = async (req, res) => {
-    if (req.session.user) {
-        var id = req.params.id;
-        db.query('select * from applications where ApplicationNo=?;', id, (err, result) => {
-            if (err) {
-                throw err
-            } else {
-                res.render('./layouts/housekeeperReviewApplications.ejs', {
-                    link1: "Review applications",
-                    link1Href: "/housekeeper",
-                    link2: "Successful Applications",
-                    link2Href: "/housekeeper/successfulapplications",
-                    sampleData: result,
-                });
-            }
-        })
-    } else {
-        res.redirect('/login')
-    }
-}
-exports.reviewApplictions = async (req, res) => {
-    var status = req.body.Status;
-    var id = req.params.appNo;
-    let params = {
-        Status: status
-    }
-    db.query('Update applications set? where ApplicationNo=?', [params, id], (err, result) => {
-        if (err) {
-            throw err
-        } else {
+        if (req.session.role === "Housekeeper") {
             db.query('select * from applications', (err, result) => {
                 if (err) {
                     return err;
@@ -62,24 +16,90 @@ exports.reviewApplictions = async (req, res) => {
                     })
                 }
             })
+        } else {
+            res.send("Unauthorized access")
         }
-    })
+    } else {
+        res.redirect('/login');
+    }
+}
+exports.viewApplications = async (req, res) => {
+    if (req.session.user) {
+        if (req.session.role === "Housekeeper") {
+            var id = req.params.id;
+            db.query('select * from applications where ApplicationNo=?;', id, (err, result) => {
+                if (err) {
+                    throw err
+                } else {
+                    res.render('./layouts/housekeeperReviewApplications.ejs', {
+                        link1: "Review applications",
+                        link1Href: "/housekeeper",
+                        link2: "Successful Applications",
+                        link2Href: "/housekeeper/successfulapplications",
+                        sampleData: result,
+                    });
+                }
+            })
+        } else {
+            res.send("Unauthorized access")
+        }
+    } else {
+        res.redirect('/login')
+    }
+}
+exports.reviewApplictions = async (req, res) => {
+    if (req.session.user) {
+        if (req.session.role === "Housekeeper") {
+            var status = req.body.Status;
+            var id = req.params.appNo;
+            let params = {
+                Status: status
+            }
+            db.query('Update applications set? where ApplicationNo=?', [params, id], (err, result) => {
+                if (err) {
+                    throw err
+                } else {
+                    db.query('select * from applications', (err, result) => {
+                        if (err) {
+                            return err;
+                        } else {
+                            res.render('./layouts/housekeeperDashboard', {
+                                link1: "Review applications",
+                                link1Href: "/housekeeper",
+                                link2: "Successful Applications",
+                                link2Href: "/housekeeper/successfulapplications",
+                                sampleData: result,
+                            })
+                        }
+                    })
+                }
+            })
+        } else {
+            res.send("Unauthorized access")
+        }
+    } else {
+        res.redirect('/login')
+    }
 }
 exports.showSuccessfulApplications = async (req, res) => {
     if (req.session.user) {
-        db.query('select * from applications where Status = "Accepted"', (err, result) => {
-            if (err) {
-                return err;
-            } else {
-                res.render('./layouts/housekeeperSuccessfulApplications', {
-                    link1: "Review applications",
-                    link1Href: "/housekeeper",
-                    link2: "Successful Applications",
-                    link2Href: "/housekeeper/successfulapplications",
-                    sampleData: result,
-                })
-            }
-        })
+        if (req.session.role === "Housekeeper") {
+            db.query('select * from applications where Status = "Accepted"', (err, result) => {
+                if (err) {
+                    return err;
+                } else {
+                    res.render('./layouts/housekeeperSuccessfulApplications', {
+                        link1: "Review applications",
+                        link1Href: "/housekeeper",
+                        link2: "Successful Applications",
+                        link2Href: "/housekeeper/successfulapplications",
+                        sampleData: result,
+                    })
+                }
+            })
+        } else {
+            res.send("Unauthorized access")
+        }
     } else {
         res.redirect('/login');
     }
